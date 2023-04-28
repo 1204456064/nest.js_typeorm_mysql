60fb587d400ed37b7fe9c4e8a1ca9974436f0000
陈文 <1204456064@qq.com>
2023-04-28 18:00:43 +0800

    增加登录前判断逻辑

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nest.js_typeorm_mysql
-nest.js和typeorm以及mysql搭建的后台
+nest.js和typeorm以及mysql搭建的后台。
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -28,9 +28,25 @@ export class UserService {
         return valid
     }
 
-    async addUser(userInfo){
-        console.log(userInfo);
-        
+    async addUser(userInfo:{ userName: string; password: string}){
+        const res = await this.productsRepository.query('select * from sys_user')
+
+        if(res.length === 0){
+            return await this.productsRepository.insert(userInfo)
+        }
+
+        let valid = true
+
+        res.forEach((item:{ id:number; userName: string; password: string })=>{
+            if(item.userName ===userInfo.userName){
+                valid =  false
+            }
+        })
+
+       if(!valid){
+           return false
+       }
+
         return await this.productsRepository.insert(userInfo)
     }
 }
